47fc681020d6fe7e416d8ff3827c6e75e7fd7063
Milagros-Alonzo <alonzomilagros24@gmail.com>
2024-11-19 22:24:52 -0500

    cambios en el config.php

--- config/config.php
+++ config/config.php
@@ -5,10 +5,10 @@ define('DB_USER', 'recetas');
 define('DB_PASSWORD', 'recetas');
 define('DB_NAME', 'recetas_web');
 
-define('BASE_PATH', dirname(__DIR__)); // Raíz del proyecto (C:/xampp/htdocs/mily/Recetas/Recetas)
-define('BASE_URL', '/mily/Recetas/Recetas/'); 
+define('BASE_PATH', dirname(__DIR__)); 
+define('BASE_URL', '/PROYECTO_FINAL/Recetas'); 
 
-define('CONEXION_DB', '/mily/Recetas/Recetas/helpers/databaseConexion.php')
+define('CONEXION_DB', '/PROYECTO_FINAL/Recetas/helpers/databaseConexion.php')
 
 /*
 *
