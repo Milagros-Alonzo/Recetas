b20caa65f92c96e437a1d0338d07c3f622d727e7
hiryonz <hiryonz024@gmail.com>
2024-12-06 01:14:56 -0500

    env

--- config/config.php
+++ config/config.php
@@ -1,12 +1,20 @@
 <?php
 
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'recetas');
-    define('DB_PASSWORD', 'recetas');
-    define('DB_NAME', 'recetas_web');
+require_once __DIR__ . '/../vendor/autoload.php';
 
-    define('BASE_PATH', dirname(__DIR__)); 
-    define('BASE_URL', '/PROYECTO_FINAL/Recetas'); 
+use Dotenv\Dotenv;
 
-    define('CONEXION_DB', '/PROYECTO_FINAL/Recetas/helpers/databaseConexion.php');
+// Cargar el archivo .env
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
+// Definir constantes con las variables de entorno
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_NAME', $_ENV['DB_NAME']);
+
+define('BASE_PATH', dirname(__DIR__));
+define('BASE_URL', $_ENV['BASE_URL']);
+
+define('CONEXION_DB', $_ENV['CONEXION_DB']);
